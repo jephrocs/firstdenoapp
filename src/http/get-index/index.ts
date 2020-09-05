ab33ce33de2b5cd18dee4b51f674b91299f4749f
Jeff C <50182734+jephrocs@users.noreply.github.com>
2020-09-04 22:55:42 -0700

    Update index.ts

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -6,6 +6,56 @@ let body = {
   
 }
 
+
+export async function handler (req: object) {
+  return {
+    statusCode: 201,
+    headers: {
+      'content-type': 'application/json; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    },
+    body: JSON.stringify({ok: true})
+  }
+}
+
+
+eliver client-side JS
+export async function handler (req: object) {
+  return {
+    headers: {
+      'content-type': 'text/javascript; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    },
+    body: 'console.log("Hello world!")',
+  }
+}
+
+
+export async function handler (req: object) {
+  return {
+    statusCode: 201,
+    headers: {
+      'content-type': 'application/json; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    },
+    body: JSON.stringify({ok: true})
+  }
+}
+
+
+eliver client-side JS
+export async function handler (req: object) {
+  return {
+    headers: {
+      'content-type': 'text/javascript; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    },
+    body: 'console.log("Hello world!")',
+  }
+}
+
+
+
 export async function handler (req: object) {
   return {
     headers: {
